bf3499d2591a5c038e71173f445276a9c7662d94
Jonas Zi <jonas.zinnoecker@gmail.com>
2022-08-06 21:09:44 +0200

    Write basic math and operate functions

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//Math operators
+
+function add (num1,num2) {
+    return num1 + num2;
+}
+
+function subtract (num1,num2) {
+    return num1 - num2;
+}
+
+function multiply (num1, num2) {
+    return num1 * num2;
+}
+
+function divide (num1, num2) {
+    return num1 / num2;
+}
+
+// Operate function
+function operate (operator, num1, num2) {
+    if (operator === "+") {
+        return add(num1,num2)
+    } else if (operator === "-") {
+        return subtract(num1,num2)
+    } else if (operator === "*") {
+        return multiply(num1,num2)
+    } else {
+        return divide(num1,num2)
+    }
+}
