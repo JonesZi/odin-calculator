5490cb1f0f30c771b5722cf8a4c5c64356d53729
Jonas Zi <jonas.zinnoecker@gmail.com>
2022-08-06 22:39:20 +0200

    Add JS to display equation

--- js/script.js
+++ js/script.js
@@ -28,3 +28,59 @@ function operate (operator, num1, num2) {
         return divide(num1,num2)
     }
 }
+
+//Populate Display
+
+function continueNumber (string) {
+    calcStr += string;
+    equation.textContent = calcStr;
+}
+
+function addFloat (string) {
+    const currentNumber = calcStr.split(" ").slice(-1).join("");
+    if (currentNumber.includes(".")) return;
+    calcStr += string;
+    equation.textContent = calcStr;
+}
+
+
+function addOperator (string) {
+    if (calcStr.slice(-1) === ".") {
+        calcStr = calcStr.slice(0,-1)
+    }
+    calcStr += ` ${string} `;
+    equation.textContent = calcStr;
+}
+
+function allClear() {
+    calcStr = "";
+    equation.textContent = calcStr;
+}
+
+function clear(string) {
+    if (string.slice(-1) === " ") {
+    calcStr = string.slice(0,-2);
+    } else {
+        calcStr = string.slice(0,-1); 
+    }
+    equation.textContent = calcStr;
+}
+
+// Variables & EventListeners
+
+let calcStr = "";
+const equation = document.querySelector(".equation");
+
+window.addEventListener("click", (e) => {
+    if (e.target.classList.contains("numbers")) {
+        continueNumber(e.target.textContent);
+    } else if (e.target.classList.contains("operators")) {
+        addOperator(e.target.textContent);
+    } else if (e.target.classList.contains("float")) {
+        addFloat(e.target.textContent);
+    } else if (e.target.classList.contains("ac")) {
+        allClear(e.target.textContent);
+    } else if (e.target.classList.contains("c")) {
+        clear(calcStr);
+    }
+});
